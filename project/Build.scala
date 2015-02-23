0b12d6f013c43eb55d85e28a7b6150dad361d326
Christian Hoffmeister <mail@choffmeister.de>
2015-02-23 12:33:53 +0100

    Remove noise form test running

--- project/Build.scala
+++ project/Build.scala
@@ -26,7 +26,7 @@ object Build extends sbt.Build {
   lazy val common = (project in file("coreci-common"))
     .settings(commonSettings: _*)
     .settings(libraryDependencies ++= Seq(
-      "ch.qos.logback" % "logback-classic" % "1.1.2",
+      "ch.qos.logback" % "logback-classic" % "1.0.13",
       "com.typesafe" % "config" % "1.2.0",
       "com.typesafe.akka" %% "akka-actor" % "2.3.7",
       "com.typesafe.akka" %% "akka-http-core-experimental" % "1.0-M3",
