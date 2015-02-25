06329009e18ea6adf385a3678de5810be463ac2c
Christian Hoffmeister <mail@choffmeister.de>
2015-02-25 19:02:08 +0100

    Extract RichConfig

--- coreci-common/src/main/scala/de/choffmeister/coreci/Config.scala
+++ coreci-common/src/main/scala/de/choffmeister/coreci/Config.scala
@@ -1,12 +1,6 @@
 package de.choffmeister.coreci
 
-import java.io.File
-import java.util.concurrent.TimeUnit
-
-import com.typesafe.config.{Config => RawConfig, ConfigException, ConfigFactory}
-
-import scala.collection.JavaConverters._
-import scala.concurrent.duration.FiniteDuration
+import com.typesafe.config.ConfigFactory
 
 case class Config(
   mongoDbServers: List[(String, Int)],
@@ -26,22 +20,5 @@ object Config {
       passwordHashAlgorithm = raw.getString("passwords.hash-algorithm").split(":", -1).toList.head,
       passwordHashAlgorithmConfig = raw.getString("passwords.hash-algorithm").split(":", -1).toList.tail
     )
-  }
-
-  implicit class RichConfig(val underlying: RawConfig) extends AnyVal {
-    def getOptionalString(path: String): Option[String] = try {
-      Some(underlying.getString(path))
-    } catch {
-      case e: ConfigException.Missing => None
-    }
-
-    def getFiniteDuration(path: String): FiniteDuration = {
-      val unit = TimeUnit.MICROSECONDS
-      FiniteDuration(underlying.getDuration(path, unit), unit)
-    }
-
-    def getStringMap(path: String): Map[String, String] = {
-      underlying.getConfig(path).root.asScala.toMap.map(e => e._1 -> e._2.unwrapped.asInstanceOf[String])
-    }
   }
 }
--- coreci-server/src/main/scala/de/choffmeister/coreci/ServerConfig.scala
+++ coreci-server/src/main/scala/de/choffmeister/coreci/ServerConfig.scala
@@ -1,11 +1,9 @@
 package de.choffmeister.coreci
 
 import java.io.File
-import java.util.concurrent.TimeUnit
 
-import com.typesafe.config.{Config => RawConfig, ConfigException, ConfigFactory}
+import com.typesafe.config.ConfigFactory
 
-import scala.collection.JavaConverters._
 import scala.concurrent.duration.FiniteDuration
 
 case class ServerConfig(
@@ -14,6 +12,8 @@ case class ServerConfig(
   webDir: Option[File])
 
 object ServerConfig {
+  import de.choffmeister.coreci.RichConfig._
+
   def load(): ServerConfig = {
     val raw = ConfigFactory.load().getConfig("coreci")
 
@@ -22,22 +22,5 @@ object ServerConfig {
       httpPort = raw.getInt("http.port"),
       webDir = raw.getOptionalString("web-dir").map(new File(_))
     )
-  }
-
-  implicit class RichConfig(val underlying: RawConfig) extends AnyVal {
-    def getOptionalString(path: String): Option[String] = try {
-      Some(underlying.getString(path))
-    } catch {
-      case e: ConfigException.Missing => None
-    }
-
-    def getFiniteDuration(path: String): FiniteDuration = {
-      val unit = TimeUnit.MICROSECONDS
-      FiniteDuration(underlying.getDuration(path, unit), unit)
-    }
-
-    def getStringMap(path: String): Map[String, String] = {
-      underlying.getConfig(path).root.asScala.toMap.map(e => e._1 -> e._2.unwrapped.asInstanceOf[String])
-    }
   }
 }
