7ec00c404ef3536d85ecc6cfd3df7687ec079037
Christian Hoffmeister <mail@choffmeister.de>
2015-05-02 16:17:05 +0200

    Allow multiple concurrent builds per worker

--- coreci-common/src/main/scala/de/choffmeister/coreci/WorkerHandler.scala
+++ coreci-common/src/main/scala/de/choffmeister/coreci/WorkerHandler.scala
@@ -3,6 +3,7 @@ package de.choffmeister.coreci
 import akka.actor._
 import akka.stream.FlowMaterializer
 import de.choffmeister.coreci.models._
+import reactivemongo.bson.BSONObjectID
 
 import scala.collection.mutable.{Map => MutableMap}
 import scala.concurrent._
@@ -15,22 +16,29 @@ object WorkerHandlerProtocol {
   case object DispatchBuild
 }
 
-case class Worker(info: Either[Int, DockerHostInfo], pings: Vector[(Long, Option[FiniteDuration])], state: WorkerState)
+case class Worker(
+  concurrency: Int,
+  builds: List[Build],
+  info: Either[Int, DockerHostInfo],
+  pings: Vector[(Long, Option[FiniteDuration])],
+  locked: Boolean)
 
 sealed trait WorkerState
 case object IdleState extends WorkerState
 case object DispatchingState extends WorkerState
 case class RunningState(build: Build) extends WorkerState
 
-class WorkerHandler(database: Database, workerUris: List[String])(implicit executor: ExecutionContext, materializer: FlowMaterializer) extends Actor with ActorLogging {
+class WorkerHandler(database: Database, workerUris: List[String])
+    (implicit executor: ExecutionContext, materializer: FlowMaterializer) extends Actor with ActorLogging {
   import WorkerHandlerProtocol._
   implicit val system = context.system
+
   context.system.scheduler.schedule(0.second, 60.second, self, UpdateInfo)
   context.system.scheduler.schedule(0.second, 15.second, self, UpdatePing)
   context.system.scheduler.schedule(0.second, 5.second, self, DispatchBuild)
 
   val builder = new Builder(database)
-  val workers = MutableMap(workerUris.map(uri => uri -> Worker(Left(0), Vector.empty, IdleState)): _*)
+  val workers = MutableMap(workerUris.map(uri => uri -> Worker(2, Nil, Left(0), Vector.empty, locked = false)): _*)
 
   def receive = {
     case UpdateInfo =>
@@ -39,51 +47,82 @@ class WorkerHandler(database: Database, workerUris: List[String])(implicit execu
         Docker.open(uri).info().onComplete {
           case Success(info) =>
             log.debug(s"Updating worker $uri state")
-            workers.update(uri, workers(uri).copy(info = Right(info)))
+            setInfo(uri, Right(info))
           case Failure(err) =>
             log.error(s"Unable to fetch worker $uri state: $err")
             workers(uri).info match {
-              case Right(_) => workers.update(uri, workers(uri).copy(info = Left(1)))
-              case Left(i) => workers.update(uri, workers(uri).copy(info = Left(i + 1)))
+              case Right(_) => setInfo(uri, Left(1))
+              case Left(i) => setInfo(uri, Left(i + 1))
             }
         }
       }
 
     case UpdatePing =>
       workers.keys.foreach { uri =>
-        val now = System.currentTimeMillis()
-        Docker.open(uri).ping().map(Some.apply)
-          .recover { case _ =>
-            log.warning(s"Pinging worker $uri failed")
-            None
+        Docker.open(uri).ping()
+          .map(Some.apply)
+          .recover { case _ => None }
+          .map {
+            case x @ Some(duration) => log.debug(s"Pinging worker $uri took ${duration.toMillis}ms"); x
+            case x @ None => log.warning(s"Pinging worker $uri failed"); x
           }
-          .foreach { time =>
-            if (time.isDefined) log.info(s"Pinging worker $uri took ${time.get.toMillis}ms")
-            val old = workers(uri)
-            workers.update(uri, old.copy(pings = capSize(old.pings :+ now -> time, 1000)))
-          }
+          .foreach(duration => addPing(uri, duration))
       }
 
     case DispatchBuild =>
-      random(workers.filter(w => w._2.info.isRight && w._2.state == IdleState)).foreach { idleWorker =>
-        workers.update(idleWorker._1, idleWorker._2.copy(state = DispatchingState))
+      val available = workers.filter(w => w._2.info.isRight && w._2.builds.length < w._2.concurrency && !w._2.locked)
 
+      random(available).foreach { case (uri, _) =>
+        lock(uri)
         database.builds.getPending()
           .recover { case err =>
             log.warning(s"Failed to get pending build: $err")
             None
           }
           .foreach {
-            case Some(pending) =>
-              log.info(s"Dispatching build ${pending.projectCanonicalName}#${pending.number} (${pending.id.stringify}) to worker ${idleWorker._1}")
-              workers.update(idleWorker._1, idleWorker._2.copy(state = RunningState(pending)))
-              builder.run(pending).foreach { finished =>
-                workers.update(idleWorker._1, idleWorker._2.copy(state = IdleState))
+            case Some(build) =>
+              log.debug(s"Dispatching build ${build.projectCanonicalName}#${build.number} (${build.id.stringify}) to worker $uri")
+              addBuild(uri, build)
+              builder.run(build).foreach { finished =>
+                removeBuild(uri, build.id)
+                self ! DispatchBuild
               }
+
+              unlock(uri)
+              self ! DispatchBuild
             case None =>
-              workers.update(idleWorker._1, idleWorker._2.copy(state = IdleState))
+              unlock(uri)
           }
       }
+  }
+
+  private def addBuild(uri: String, build: Build) = {
+    val w = workers(uri)
+    workers.update(uri, w.copy(builds = w.builds :+ build))
+  }
+
+  private def removeBuild(uri: String, buildId: BSONObjectID) = {
+    val w = workers(uri)
+    workers.update(uri, w.copy(builds = w.builds.filter(_.id != buildId)))
+  }
+
+  private def lock(uri: String) = {
+    workers.update(uri, workers(uri).copy(locked = true))
+  }
+
+  private def unlock(uri: String) = {
+    workers.update(uri, workers(uri).copy(locked = false))
+  }
+
+  private def addPing(uri: String, duration: Option[FiniteDuration]) = {
+    val w = workers(uri)
+    val now = System.currentTimeMillis()
+    workers.update(uri, w.copy(pings = capSize(w.pings :+ now -> duration, 1000)))
+  }
+
+  private def setInfo(uri: String, info: Either[Int, DockerHostInfo]) = {
+    val w = workers(uri)
+    workers.update(uri, w.copy(info = info))
   }
 
   private def random[K, V](map: MutableMap[K, V]): Option[(K, V)] = map.size match {
