33c704c18f79c2c3119c15271c5533f1c18daac3
Christian Hoffmeister <mail@choffmeister.de>
2015-06-02 08:08:44 +0200

    Fix test timeouts again

--- coreci-server/src/test/scala/de/choffmeister/coreci/http/Specs2RouteTest.scala
+++ coreci-server/src/test/scala/de/choffmeister/coreci/http/Specs2RouteTest.scala
@@ -3,7 +3,11 @@ package de.choffmeister.coreci.http
 import akka.http.scaladsl.testkit._
 import de.choffmeister.coreci.JsonProtocol
 
+import scala.concurrent.duration._
+
 trait Specs2RouteTest extends TestFrameworkInterface with RouteTest with JsonProtocol {
+  implicit val timeout = RouteTestTimeout(5.seconds)
+
   override def cleanUp(): Unit = {}
 
   override def failTest(msg: String): Nothing = {
